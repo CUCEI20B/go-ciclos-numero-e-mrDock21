6900905cca5e8a1b2e17713a5e80379a74f7ead3
mrDock21 <danes_warrior@hotmail.com>
2020-09-29 13:43:04 -0500

    Implementado el calculo de 'e'

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func factorial(number int) float32 {
+	var res float32 = 0
+
+	if number > 1 {
+		res = float32(number) * float32(number - 1)
+		for i := number - 2; i > 0; i-- {
+			res = res * float32(i)
+		}
+	} else {
+		res = 1
+	}
+
+	return res
+}
+
+func main() {
+	var input string
+	var res float32 = 0
+
+	// Read from console: number of iterations
+	fmt.Scan(&input)
+
+	// Scan values from input string
+	N, _ := strconv.Atoi(input)
+
+	for i := 0; i <= N; i++ {
+		fact := factorial(i)
+		res += float32(1) / fact
+	}
+
+	fmt.Println(res)
 }
